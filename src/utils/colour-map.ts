7d203c1c64474bf26eb3e59d8bb2e4aa72875583
rosieprom <rosalinda.prom@gmail.com>
2024-12-13 09:55:25 +1100

    feat: added confetti, fix layout and wrote tests

---
+++ src/utils/colour-map.ts
@@ -0,0 +1,71 @@
+export type SegmentFill = {
+  text: string;
+  background: string;
+};
+
+export const colorMap: SegmentFill[] = [
+  {
+    text: "#000",
+    background: "#90d8b2",
+  },
+  {
+    text: "#000",
+    background: "#8dd2dd",
+  },
+  {
+    text: "#000",
+    background: "#8babf1",
+  },
+  {
+    text: "#000",
+    background: "#8b95f6",
+  },
+  {
+    text: "#000",
+    background: "#9b8bf4",
+  },
+  {
+    text: "#000",
+    background: "#faaf90",
+  },
+  {
+    text: "#000",
+    background: "#fcc9b5",
+  },
+  {
+    text: "#000",
+    background: "#d9e4ff",
+  },
+  {
+    text: "#000",
+    background: "#b3c7f7",
+  },
+  {
+    text: "#000",
+    background: "#8babf1",
+  },
+  {
+    text: "#000",
+    background: "#b9e192",
+  },
+  {
+    text: "#000",
+    background: "#cfebb6",
+  },
+  {
+    text: "#000",
+    background: "#b3c7f7",
+  },
+  {
+    text: "#000",
+    background: "#f8b8d0",
+  },
+  {
+    text: "#000",
+    background: "#f194b8",
+  },
+  {
+    text: "#000",
+    background: "#9b8bf4",
+  },
+];
